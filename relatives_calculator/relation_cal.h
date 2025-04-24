19638d02766fdd0e3f9a2756595d9005a7921fb6
xbdieiong <973003575@qq.com>
2025-04-24 18:04:55 +0800

    update rel_cal

--- relatives_calculator/relation_cal.h
+++ relatives_calculator/relation_cal.h
@@ -1,7 +1,6 @@
 #ifndef __RRLATION_CAL_H__
 #define __RRLATION_CAL_H__
 
-
 /*********************
  *      INCLUDES
  *********************/
@@ -11,13 +10,14 @@
 #include <stdio.h>
 #include <ctype.h>
 
-#define REL_NODE_SUCCESS  1
-#define REL_NODE_ERROR 0
+#define MAX_REL_LEN 256
 
-#define REL_CALING 0
-#define REL_CAIL_COMPELETE 1
-
-#define MAX_TREE_LEN 256
+typedef enum relation_cal_status_e {
+    STATUS_INIT,
+    STATUS_INPUTTING,
+    STATUS_CALCULATED,
+    STATUS_XXX
+}relation_cal_status_t;
 
 typedef enum gender_e {
     GENDER_MALE,
@@ -25,51 +25,65 @@ typedef enum gender_e {
     GENDER_UNKNOWN
 }gender_t;
 
-typedef enum relation_type_e{
-    REL_FATHER,
-    REL_MOTHER,
-    REL_SON,
-    REL_DAUGHTER,
-    REL_WIFE,
-    REL_HUSBAND,
-    REL_ELDER_BROTHER,
-    REL_YOUNGER_BROTHER, 
-    REL_ELDER_SISTER,
-    REL_YOUNGER_SISTER,
-    REL_NULL
-}relation_type_t;
+typedef enum relation_type_e {
+    REL_SELF,                        // 自己
+    REL_FATHER,                      // 父亲
+    REL_MOTHER,                      // 母亲
+    REL_SON,                         // 儿子
+    REL_DAUGHTER,                    // 女儿
+    REL_WIFE,                        // 妻子
+    REL_HUSBAND,                     // 丈夫
+    REL_ELDER_BROTHER,               // 哥哥
+    REL_YOUNGER_BROTHER,             // 弟弟
+    REL_ELDER_SISTER,                // 姐姐
+    REL_YOUNGER_SISTER,              // 妹妹
+    REL_GRANDFATHER,                 // 祖父
+    REL_GRANDMOTHER,                 // 祖母
+    REL_UNCLE,                       // 伯父（父亲的哥哥）
+    REL_AUNT,                        // 姑母（父亲的姐妹）
+    REL_COUSIN_MALE,                 // 堂兄弟（父系）
+    REL_COUSIN_FEMALE,               // 堂姐妹（父系）
+    REL_GREAT_GRANDFATHER,           // 曾祖父（父系）
+    REL_GREAT_GRANDMOTHER,           // 曾祖母（父系）
+    REL_MATERNAL_GRANDFATHER,        // 外祖父
+    REL_MATERNAL_GRANDMOTHER,        // 外祖母
+    REL_MATERNAL_GREAT_GRANDFATHER,  // 外曾祖父
+    REL_MATERNAL_GREAT_GRANDMOTHER,  // 外曾祖母
+    REL_FIRST_COUSIN_ONCE_REMOVED_MALE,   // 表侄
+    REL_FIRST_COUSIN_ONCE_REMOVED_FEMALE, // 表侄女
+    REL_SECOND_COUSIN_MALE,          // 再从兄弟
+    REL_SECOND_COUSIN_FEMALE,        // 再从姐妹
+    REL_GREAT_UNCLE,                 // 叔祖父（祖父的弟弟）
+    REL_GREAT_AUNT,                  // 姑祖母（祖父的姐妹）
+    REL_MATERNAL_UNCLE,              // 舅父（母亲的兄弟）
+    REL_MATERNAL_AUNT,               // 姨母（母亲的姐妹）
+    REL_NEPHEW,                      // 侄子
+    REL_NIECE,                       // 侄女
+    REL_SON_IN_LAW,                  // 媳妇
+    REL_DAUGHTER_IN_LAW,             // 女婿
+    REL_GRANDSON,                    // 孙子
+    REL_GRANDDAUGHTER,               // 孙女
+    REL_GREAT_GRANDSON,              // 曾孙子
+    REL_GREAT_GRANDDAUGHTER,         // 曾孙女
+    REL_COUSIN_SON,                  // 堂侄/表侄
+    REL_COUSIN_DAUGHTER,             // 堂侄女/表侄女
+    REL_GRAND_NEPHEW,                // 孙侄（兄弟的孙子）
+    REL_GRAND_NIECE,                 // 孙侄女（兄弟的孙女）
+    REL_NULL                         // 未知
+} relation_type_t;
 
-typedef struct relation_node_s
-{
-    enum relation_type_e relation;
-    struct relation_node_s *next;
-}relation_node_t;
-
-
-typedef struct btnm_relation_s
+typedef struct btnm_relation_s 
 {
     const char *btnm_text;
     relation_type_t relation;
     gender_t gender;
-}btnm_relation_t;
+} btnm_relation_t;
 
-
-typedef struct relation_tree_s
+typedef struct relation_call_s
 {
-    enum gender_e gender;
     const char *call;
-    
-    struct relation_tree_s *father;
-    struct relation_tree_s *mother;
-    struct relation_tree_s *wife;
-    struct relation_tree_s *husband;
-    struct relation_tree_s *son;
-    struct relation_tree_s *daughter;
-    struct relation_tree_s *elder_bro;
-    struct relation_tree_s *younger_bro;
-    struct relation_tree_s *elder_sis;
-    struct relation_tree_s *younger_sis;
-}relation_tree_t;
+    relation_type_t relation;
+}relation_call_t;
 
 typedef struct relation_cal_s
 {
@@ -81,11 +95,18 @@ typedef struct relation_cal_s
         lv_obj_t *screen;
     }ui;
     
-    struct relation_node_s *rel_list;
-    relation_tree_t mine;
+    relation_type_t rel_list[MAX_REL_LEN];
+    int rel_count;
+    relation_cal_status_t status;
 
-    uint8_t status;
 }relation_cal_t;
+
+
+typedef struct relation_transformation_s {
+    relation_type_t from;
+    relation_type_t to;
+    relation_type_t result;
+} relation_transformation_t;
 
 void relation_cal_app_create(void);
 
