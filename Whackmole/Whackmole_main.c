d1ec51bdd13ae3ceeb36d93373237b2374c368b1
Hu_JW <1748239230@qq.com>
2025-07-09 17:02:36 +0800

    suceessfully running on esp
    Signed-off-by: Hu_JW <1748239230@qq.com>
    
    suceessfully on esp
    
    successfully on esp
    
    successfully running on esp
    
    successfully running on esp
    
    update readme
    
    update readme
    
    update readme
    
    code format

--- Whackmole/Whackmole_main.c
+++ Whackmole/Whackmole_main.c
@@ -1,11 +1,18 @@
 // include NuttX headers
 #include <nuttx/config.h>
 #include <unistd.h>
+
+#ifdef CONFIG_LV_USE_NUTTX_LIBUV
 #include <uv.h>
+#endif
 
 // include lvgl headers
 #include <lvgl/lvgl.h>
+
 #include "Whackmole.h"
+
+#ifdef CONFIG_LV_USE_NUTTX_LIBUV
+
 static void lv_nuttx_uv_loop(uv_loop_t* loop, lv_nuttx_result_t* result)
 {
     lv_nuttx_uv_t uv_info;
@@ -26,15 +33,18 @@ static void lv_nuttx_uv_loop(uv_loop_t* loop, lv_nuttx_result_t* result)
     lv_nuttx_uv_deinit(&data);
 }
 
-#include "Whackmole.h"
+#endif
 
 int main(int argc, FAR char* argv[])
 {
     // init lvgl
     lv_nuttx_dsc_t info;
     lv_nuttx_result_t result;
+
+#ifdef CONFIG_LV_USE_NUTTX_LIBUV
     uv_loop_t ui_loop;
     lv_memset(&ui_loop, 0, sizeof(uv_loop_t));
+#endif
 
     if (lv_is_initialized()) {
         LV_LOG_ERROR("LVGL already initialized! aborting.");
@@ -44,6 +54,11 @@ int main(int argc, FAR char* argv[])
     lv_init();
 
     lv_nuttx_dsc_init(&info);
+
+#ifdef CONFIG_LV_USE_NUTTX_LCD
+    info.fb_path = "/dev/lcd0";
+#endif
+
     lv_nuttx_init(&info, &result);
 
     if (result.disp == NULL) {
@@ -53,8 +68,20 @@ int main(int argc, FAR char* argv[])
 
     app_create();
 
+#ifdef CONFIG_LV_USE_NUTTX_LIBUV
     // refresh lvgl ui
     lv_nuttx_uv_loop(&ui_loop, &result);
+#endif
+
+    while (1) {
+        uint32_t idle;
+        idle = lv_timer_handler();
+
+        /* Minimum sleep of 1ms */
+
+        idle = idle ? idle : 1;
+        usleep(idle * 1000);
+    }
 
     lv_nuttx_deinit(&result);
     lv_deinit();
---
+++ Whackmole/pic/MiSans_Semibold.h
@@ -0,0 +1,16 @@
+#ifndef SEM_H
+#define SEM_H
+
+#include "lvgl.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern const lv_font_t MiSans_Semibold;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif 
