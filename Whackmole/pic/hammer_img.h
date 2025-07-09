d20f21ac50feef2da71fc6bae43e4a344708331e
Hu_JW <1748239230@qq.com>
2025-07-09 17:02:36 +0800

    suceessfully running on esp
    suceessfully on esp
    
    successfully on esp
    
    successfully running on esp
    
    successfully running on esp
    
    update readme
    
    update readme
    
    update readme
    
    code format
    
    Signed-off-by: Hu_JW <1748239230@qq.com>

---
+++ Whackmole/pic/hammer_img.h
@@ -0,0 +1,16 @@
+#ifndef HAMMER_IMG_H
+#define HAMMER_IMG_H
+
+#include "lvgl.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern const lv_img_dsc_t hammer_img;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* HAMMER_IMG_H */
---
+++ Whackmole/pic/mole_image.h
@@ -0,0 +1,16 @@
+#ifndef MOLE_IMG_H
+#define MOLE_IMG_H
+
+#include "lvgl.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern const lv_img_dsc_t mole_image;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* MOLE_IMG_H */
